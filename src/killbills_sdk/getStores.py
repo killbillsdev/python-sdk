79bdeef10189522891ba196c5d43a59172aebce6
flozonn <florian.breton1@icloud.com>
2023-10-24 11:54:48 +0200

    [GET STORES] add user control on offset and limit

--- src/killbills_sdk/getStores.py
+++ src/killbills_sdk/getStores.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_stores(env, api_key):
+def get_stores(env, api_key, limit=100, offset=0):
     
     if not env or env == '' or env is None:
         raise ValueError('No environment specified')
@@ -9,9 +9,12 @@ def get_stores(env, api_key):
 
     base_url = f'https://w.{"killbills.co" if env == "prod" else f"{env}.killbills.dev"}/stores'
     headers = {'Authorization': api_key}
-
-    response = requests.get(base_url, headers=headers)
-    if 'error' in response.json() :
+    params = {
+        'limit': limit,
+        'offset': offset
+    }
+    response = requests.get(base_url, headers=headers, params=params)
+    if response.status_code != 200:
         raise ValueError(f'Error: {response.status_code} - {response.text}')
-    return response.json().get('items')
+    return response.json()
 
